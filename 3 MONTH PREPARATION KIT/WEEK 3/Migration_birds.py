ec1abbd55432a8448936fba115cfad5440eecd56
Scavenger <valhallalexander@gmail.com>
2024-08-27 19:13:22 -0600

    Actualizacion de proyecto

---
+++ 3 MONTH PREPARATION KIT/WEEK 3/Migration_birds.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    birds = [0]*5
+    for bird in arr:
+        birds[bird-1] += 1
+
+    collections = []
+
+    for i in range(len(birds)):
+        if birds[i] == max(birds):
+            collections.append(i+1)
+
+    return collections[0]
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
